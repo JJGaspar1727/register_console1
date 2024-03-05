5fa67462e50d7ad70d860f054b3566c73f952523
JJGaspar1727 <johnjosephgaspar@gmail.com>
2024-03-05 14:31:49 +0800

    second commit

--- jsregister.js
+++ jsregister.js
@@ -10,17 +10,37 @@
         // ["apple", "banana", "grape"]
         // ]
 
-        console.log("hello world", 123, true,["apple", "banana", "grape"]);
+        //console.log("hello world", 123, true,["apple", "banana", "grape"]);
         //console.error("Hello World");
         //console.warn("Hello World");
         //console.table(["apple", "banana", "grape"]);
         
         let txtUsername = document.querySelector("#txtUsername");
         let btnRegister = document.querySelector("#btnRegister");
+        let txtPassword = document.querySelector("#txtPassword");
+        let txtConfirmPassword = document.querySelector("#txtConfirmPassword");
         
-        btnRegister.onclick =function () {
-            register(txtUsername.value)
+        btnRegister.onclick = function () {
+            register(txtUsername.value, 
+                txtPassword.value, 
+                txtConfirmPassword.value)
         }
-        function register (username) {
-            console.log(username)
+        function register (username, password, confirmpassword) {
+            // if...else...
+            // conditional operators (greater than >, less than <, equal ==, not !, or ||
+            // greater than or equal >=, less than or equal<=
+            // not equal !=
+
+        if(password == confirmpassword && password != "" && confirmpassword != "") {
+                return console.log("Password match!")
+        } else if (password == "" || confirmpassword =="") {
+            return console.log("Password must not be empty")
+        }
+        else {
+            return console.log("Password does not match")
+        }
+            
+        
+        
+           //console.log(username, password, confirmpassword)
         }
